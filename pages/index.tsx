d17a63a16821cdd264e575b7c6ecf3894adb310b
Karan Sanjeev <yo@alphaman.me>
2021-09-25 17:19:35 +0530

    Basic login setup done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <section className="section container">
+        <h1 className="title">Logged in</h1>
+        <h2>{session.user?.name}</h2>
+        <h3>{session.user?.email}</h3>
+        <button className="button is-info" onClick={() => signOut()}>Logout</button>
+      </section>
+    )
+  }
+
   return (
     <div>
       <Head>
@@ -10,7 +24,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
+      <section className="hero is-fullheight is-info">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Zepplin Meme</h1>
+            <h2 className="subtitle">Login with your official email ID to make submissions</h2>
+            <a href="#" onClick={() => signIn('google')} className="button is-outlined is-medium is-info is-inverted">Login with Google</a>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
