d49ca657c97ca7a7bebd734827eafcf1e3d79a2f
Karan Sanjeev <yo@alphaman.me>
2021-09-25 16:56:25 +0530

    Add next-auth:

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export default NextAuth({
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_OAUTH_ID as string,
+            clientSecret: process.env.GOOGLE_OAUTH_SECRET as string
+        })
+    ],
+})
