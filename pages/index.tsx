1b91c3f6bf45a86d8ede46ded197b31966bd2c70
Karan Sanjeev <yo@alphaman.me>
2021-09-26 17:54:16 +0530

    Reformat index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,40 +3,40 @@ import { signIn, getSession } from 'next-auth/react'
 import Meta from '../components/Meta';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Meta />
+    return (
+        <div>
+            <Meta />
 
-      <section className="hero is-fullheight is-info">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Zepplin Meme</h1>
-            <h2 className="subtitle">Login with your official email ID to make submissions</h2>
-            <a href="#" onClick={() => signIn('google')} className="button is-outlined is-medium is-info is-inverted">Login with Google</a>
-          </div>
+            <section className="hero is-fullheight is-info">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">Zepplin Meme</h1>
+                        <h2 className="subtitle">Login with your official email ID to make submissions</h2>
+                        <a href="#" onClick={() => signIn('google')} className="button is-outlined is-medium is-info is-inverted">Login with Google</a>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  )
+    )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
+    const session = await getSession(context);
 
-  if (session) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: '/dashboard'
-      }
-    };
-  }
-
-  return {
-    props: {
-      session
+    if (session) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: '/dashboard'
+            }
+        };
     }
-  }
+
+    return {
+        props: {
+            session
+        }
+    }
 }
 
 export default Home
