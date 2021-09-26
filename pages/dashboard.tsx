52f9520732e7f868de2add33c24ca54b7e6173c7
Karan Sanjeev <yo@alphaman.me>
2021-09-26 11:57:37 +0530

    Meme form done

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -19,12 +19,26 @@ const MemeForm = () => {
                         </p>
                     </div>
                     <div className="card-content">
-                        <div className="field">
-                            <label htmlFor="ighandle" className="label">Your IG handle</label>
-                            <div className="control">
-                                <input type="text" className="input" name="ighandle" id="ighandle" required />
+                        <form>
+                            <div className="field">
+                                <label htmlFor="ighandle" className="label">Your IG handle</label>
+                                <div className="control">
+                                    <input type="text" className="input" name="ighandle" id="ighandle" placeholder="@myInstaHandle" required />
+                                </div>
                             </div>
-                        </div>
+                            <div className="field">
+                                <label htmlFor="meme" className="button is-outlined is-fullwidth">Select your Meme</label>
+                                <div className="control">
+                                    <input type="file" className="button is-primary is-outlined is-sr-only" name="meme" id="meme" required />
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="field">
+                                <div className="control">
+                                    <input type="submit" value="Submit Meme" className="button is-primary is-fullwidth is-outlined" />
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
