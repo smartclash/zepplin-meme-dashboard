4b34e7bd97a3a4a93e3e53e2cdd0862f2d197cf8
Karan Sanjeev <yo@alphaman.me>
2021-09-26 11:14:15 +0530

    Work towards meme submission

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -8,70 +8,99 @@ interface DashboardProps {
     user: User
 }
 
+const MemeForm = () => {
+    return (
+        <section className="section">
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            Submit your Meme!
+                        </p>
+                    </div>
+                    <div className="card-content">
+                        <div className="field">
+                            <label htmlFor="ighandle" className="label">Your IG handle</label>
+                            <div className="control">
+                                <input type="text" className="input" name="ighandle" id="ighandle" required />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
 const Dashboard = ({ user }: DashboardProps) => {
     return (
         <div>
             <Meta title="Dashboard" />
             <Navbar />
 
-            <section className="section">
-                <div className="container">
-                    <h1 className="is-size-3 is-capitalized">
-                        Welcome, {user.name.toLocaleLowerCase()} - <a onClick={() => signOut()}>Logout</a>
-                    </h1>
+            <div className="columns">
+                <div className="column is-8 is-offset-2">
+                    <section className="section">
+                        <div className="container">
+                            <h1 className="is-size-3 is-capitalized">
+                                Welcome, {user.name.toLocaleLowerCase()} - <a onClick={() => signOut()}>Logout</a>
+                            </h1>
+                        </div>
+                    </section>
+                    {!user.completed && <MemeForm />}
+                    <section className="section">
+                        <div className="container">
+                            <div className="columns">
+                                <div className="column is-4">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <p className="card-header-title">Rules</p>
+                                        </div>
+                                        <div className="card-content">
+                                            <div className="content">
+                                                You gotta follow some rules to make sure your meme is accepted into this competition.
+                                            </div>
+                                        </div>
+                                        <div className="card-footer">
+                                            <a href="https://firebasestorage.googleapis.com/v0/b/zepplin21-fa4fd.appspot.com/o/meme.jpg?alt=media&token=a73cc9ca-ae9c-463e-90db-51ac17b6e918" className="card-footer-item" target="_blank">Read Rules ↗</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column is-4">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <p className="card-header-title">Meme IG Handle</p>
+                                        </div>
+                                        <div className="card-content">
+                                            <div className="content">
+                                                Submissions made by participants will be posted at the official Zepplin's Meme Instagram handle.
+                                            </div>
+                                        </div>
+                                        <div className="card-footer">
+                                            <a href="" className="card-footer-item" target="_blank">Instagram ↗</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column is-4">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <p className="card-header-title">Discord</p>
+                                        </div>
+                                        <div className="card-content">
+                                            <div className="content">
+                                                Join Zepplin's Discord server and have fun with fellow participants. Ask questions and get them sorted out too.
+                                            </div>
+                                        </div>
+                                        <div className="card-footer">
+                                            <a href="https://discord.gg/fH6fuJ5WWk" className="card-footer-item" target="_blank">Discord ↗</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
-            </section>
-            <section className="section">
-                <div className="container">
-                    <div className="columns">
-                        <div className="column is-4">
-                            <div className="card">
-                                <div className="card-header">
-                                    <p className="card-header-title">Rules</p>
-                                </div>
-                                <div className="card-content">
-                                    <div className="content">
-                                        You gotta follow some rules to make sure your meme is accepted into this competition.
-                                    </div>
-                                </div>
-                                <div className="card-footer">
-                                    <a href="" className="card-footer-item">Read Rules ↗</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column is-4">
-                            <div className="card">
-                                <div className="card-header">
-                                    <p className="card-header-title">Meme IG Handle</p>
-                                </div>
-                                <div className="card-content">
-                                    <div className="content">
-                                        Submissions made by participants will be posted at the official Zepplin's Meme Instagram handle.
-                                    </div>
-                                </div>
-                                <div className="card-footer">
-                                    <a href="" className="card-footer-item">Instagram ↗</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column is-4">
-                            <div className="card">
-                                <div className="card-header">
-                                    <p className="card-header-title">Zepplin Website</p>
-                                </div>
-                                <div className="card-content">
-                                    <div className="content">
-                                        Visit the official Zepplin website to know various other events conducted.
-                                    </div>
-                                </div>
-                                <div className="card-footer">
-                                    <a href="" className="card-footer-item">Website ↗</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            </div>
         </div>
     )
 }
