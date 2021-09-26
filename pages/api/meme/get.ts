0b41cff2f462d3c2ccf9209bab3e7259ca9a0cec
Karan Sanjeev <yo@alphaman.me>
2021-09-26 14:43:15 +0530

    API to get meme of loggedin user

---
+++ pages/api/meme/get.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import deta from "../../../components/deta";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req });
+    if (!session) {
+        return res.status(404);
+    }
+
+    const db = deta.Base('users');
+    const user = await db.get(session.user?.email as string);
+
+    if (user && !user.completed) {
+        return res.status(404);
+    }
+
+    const drive = deta.Drive('memes');
+    const meme = await drive.get(session.user?.email as string);
+
+    res.setHeader('Content-Type', meme?.type as string);
+    return res.send(meme?.stream());
+}
+
+export default handler;
