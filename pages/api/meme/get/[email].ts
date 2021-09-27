9e2b695f1747f136101051aa78f1107fa6b21f65
Karan Sanjeev <yo@alphaman.me>
2021-09-27 07:52:39 +0530

    Get meme of any user by email

---
+++ pages/api/meme/get/[email].ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import deta from "@/components/deta";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const email = req.query.email as string;
+
+    const drive = deta.Drive('memes');
+    const meme = await drive.get(email as string);
+
+    if (!meme) {
+        return res.status(404).send('');
+    }
+
+    res.setHeader('Content-Type', meme?.type as string);
+    return res.status(200).send(meme?.stream());
+}
+
+export default handler;
